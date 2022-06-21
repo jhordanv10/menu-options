97577bfc9583575d665d9d33e1535ef88fe46d57
Daniela Buitrago <lauridani1@gmail.com>
2022-06-21 17:32:32 -0500

    change color

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -20,21 +20,30 @@
       </v-col>
     <v-col cols="6" class="mt-2">
       Estoy en la propiedad: {{this.getName}} del {{figure}}
+    
+       <!-- {{inf}} -->
+
+      <span class="colorPicker"><input id="cone-color" type="color" value="#ff0000" onchange="this.changeColor()"/><br/>Color</span>
+
+      {{this.color}}
     </v-col>
     </v-row>
   </v-navigation-drawer>
 </template>
 
 <script>
+import * as THREE from "three";
 import { Icon } from "@iconify/vue2";
 
 export default {
   props: {
     figure: String,
-    info: Array
+    inf: Array,
+    material: THREE.MeshBasicMaterial,
   },
   data() {
     let getName='';
+    let color= '';
     return {
       drawer: true,
       properties: [
@@ -44,18 +53,44 @@ export default {
         { id: 4, name: "Scale", icon: "mdi-relative-scale" },
       ],
       getName: getName,
+      color: color,
     };
   },
   components: {
     Icon,
   },
-  mounted() {},
+  mounted() {
+    // this.setMaterial();
+  },
   methods: {
     drawer() {
         this.drawer = !this.drawer
     },
     sendData(name){
       this.getName= name;
+    },
+    changeColor(){
+      // var guiControls = new function() {
+      //   this.color = material.color.getStyle();
+      // }();
+
+      // var gui = new dat.GUI();
+      // gui
+      //   .addColor(guiControls, "color")
+      //   .listen()
+      //   .onChange(function(e) {
+      //     selected.material.color.setStyle(e);
+      //   });
+
+
+      // const colorInput = document.getElementById( 'cone-color' );
+				// colorInput.addEventListener( 'input', function () {
+          this.color=document.getElementById("cone-color").value;
+					material.color.set(this.color);
+
+				// } );
+
+      
     }
   },
 };
