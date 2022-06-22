714dc48181f376b089fd8329f3e7413c758a86cf
jhordanv10 <jhordanvilla1000@gmail.com>
2022-06-22 12:02:08 -0500

    Se crea componente Material

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -25,76 +25,9 @@
           <h1>Principal</h1>
         </div>
 
-        <!-- -------------------------------- Material --------------------------------------->
-        <div v-if="getName === 'Material'">
-          <!-- color -->
-          <v-container v-if="this.info.material.color">
-            <span class="colorPicker"
-              ><input
-                :id="figure+'-color'"
-                type="color"
-                value="#ff0000"
-                @change="changeColor"
-              /><br />Color</span
-            >
-          </v-container>
-          <!-- Wireframe -->
-          <v-container>
-            <v-row>
-              <label
-                :class="wireframe === true ? 'green--text' : 'red--text'"
-                >{{
-                  wireframe === true ? "Wireframe true" : "Wireframe false"
-                }}</label
-              >
-            </v-row>
-            <v-row>
-              <v-switch
-                :class="
-                  wireframe === true ? 'text--green my-0' : 'text--red my-0'
-                "
-                @click="changeWireframe"
-                v-model="wireframe"
-                :color="wireframe === true ? 'green' : 'red'"
-                hide-details
-              ></v-switch>
-            </v-row>
-          </v-container>
-          <v-container>
-            <v-row>
-              <v-col class="d-flex px-0" cols="12">
-                <v-select
-                  v-model="side"
-                  @change="changeSide"
-                  :items="items"
-                  label="Side"
-                ></v-select>
-              </v-col>
-            </v-row>
-          </v-container>
-          <v-container justify="center">
-            <v-row>
-              <v-col class="d-flex px-0 mx-0 justify-center" cols="12">
-                <v-btn
-                  v-model="visible"
-                  @click="changeVisible"
-                  class="mx-2"
-                  fab
-                  dark
-                  :color="visible === true ? 'red' : 'green'"
-                >
-                  <v-icon dark>
-                    {{
-                      visible === true
-                        ? "mdi-eye-off-outline"
-                        : "mdi-eye-outline"
-                    }}</v-icon
-                  >
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-container>
-        </div>
+        <Material v-if="getName === 'Material'" :info="info" :figure="figure" />
+
+
 
         <!-- -------------------------------- Rotation --------------------------------------->
         <div v-if="getName === 'Rotation'">
@@ -120,6 +53,7 @@
 <script>
 import * as THREE from "three";
 import { Icon } from "@iconify/vue2";
+import Material from "../components/Atoms/Material.vue"
 
 export default {
   props: {
@@ -128,71 +62,31 @@ export default {
   },
   data() {
     let getName = "";
-    let color = "";
     return {
       drawer: true,
       getName: getName,
-      wireframe: false,
-      side: null,
-      visible: true,
-      items: [
-        { value: "0", text: "Font" },
-        { value: "1", text: "Back" },
-      ],
       properties: [
         { id: 1, name: "Material", icon: "mdi-palette-swatch" },
         { id: 2, name: "Rotation", icon: "mdi-rotate-3d-variant" },
         { id: 3, name: "Position", icon: "mdi-chart-ppf" },
         { id: 4, name: "Scale", icon: "mdi-relative-scale" },
       ],
-      getName: getName,
-      color: color,
+      
     };
   },
   components: {
     Icon,
+    Material,
   },
-  mounted() {
-    
-    console.log(this.info.material);
-  },
-
   methods: {
     sendData(name) {
       this.getName = name;
     },
-
-    changeWireframe() {
-      if (this.wireframe === true) {
-        this.info.material.wireframe = true;
-      } else {
-        this.info.material.wireframe = false;
-      }
-    },
-    changeSide() {
-      if (this.side === "0") {
-        this.info.material.side = 0;
-      } else if (this.side === "1") {
-        this.info.material.side = 1;
-      }
-    },
-    changeVisible() {
-      this.visible = !this.visible;
-
-      if (this.visible === true) {
-        this.info.material.visible = true;
-      } else {
-        this.info.material.visible = false;
-      }
-    },
-    sendData(name) {
-      this.getName = name;
-    },
-    changeColor() {
-      this.color = document.getElementById(`${this.figure}-color`).value;
-      this.info.material.color.set(this.color);
-    },
   },
+  mounted() {
+    console.log(this.info.material);
+  },
+
 };
 </script>
 
