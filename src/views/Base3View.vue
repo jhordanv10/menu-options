e90158b96137fc79840cb97ed79ea5a078a360a4
Daniela Buitrago <lauridani1@gmail.com>
2022-06-30 12:12:25 -0500

    change texture

--- src/views/Base3View.vue
+++ src/views/Base3View.vue
@@ -11,13 +11,16 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 import Menu from "../components/Menu.vue";
 
+
 export default {
+
+
   components: {
     Menu,
   },
-  data() {
+  data: function () {
     //Scene
-    let scene = new THREE.Scene();
+    const scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf0f0f0);
 
     //Camera
@@ -34,21 +37,22 @@ export default {
 
     //Cone
     const geometry = new THREE.ConeGeometry(1.5, 2, 4);
-    let material = new THREE.MeshStandardMaterial({
+
+    const material = new THREE.MeshStandardMaterial({
       color: 0xff0000,
+      flatShading: true,
       metalness: 1.0,
       roughness: 0.5,
       clearcoat: 1.0,
       clearcoatRoughness: 0.03,
       sheen: 0.5,
     });
-
     // const texture = new THREE.TextureLoader().load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/259155/THREE_gates.jpg');
     // const material = new THREE.MeshStandardMaterial({
     //   map: texture,
     // });
 
-    let cone = new THREE.Mesh(geometry, material);
+    const cone = new THREE.Mesh(geometry, material);
 
     //Ligths
     const AmbientalLigth = new THREE.AmbientLight(0xffffff, 1);
@@ -56,9 +60,9 @@ export default {
 
 
     // instantiate a texture loader
-    let textureToShow = 0;
+    const textureToShow = 0;
 
-    let arr = [
+    const arr = [
       'https://s3-us-west-2.amazonaws.com/s.cdpn.io/259155/THREE_gates.jpg',
       'https://s3-us-west-2.amazonaws.com/s.cdpn.io/259155/THREE_crate1.jpg',
       'https://s3-us-west-2.amazonaws.com/s.cdpn.io/259155/THREE_crate2.jpg'
@@ -68,11 +72,11 @@ export default {
       scene: scene,
       camera: camera,
       renderer: renderer,
+      material: material,
       cone: cone,
       controls: [],
       AmbientalLigth: AmbientalLigth,
       DirectionalLigth: DirectionalLigth,
-      material: material,
       geometry: geometry,
       // The textures to use
       arr: arr,
@@ -80,32 +84,32 @@ export default {
     };
   },
 
-  created() {
-    //Camera
-    this.camera.position.z = 6;
-    this.scene.add(this.camera);
-
-    //Cone
-    this.cone.rotation.set(0.5, 1, 0);
-    this.scene.add(this.cone);
-
-    //Ligth
-    this.scene.add(this.AmbientalLigth);
-    this.DirectionalLigth.position.set(3, 3, 3);
-    this.scene.add(this.DirectionalLigth);
-
-    //Controls
-    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    this.controls.enableDamping = true;
-  },
-
   mounted() {
+    this.created();
     this.$refs.canvas.appendChild(this.renderer.domElement);
     this.animate();
     this.changeTexture()
   },
 
   methods: {
+    created() {
+      //Camera
+      this.camera.position.z = 6;
+      this.scene.add(this.camera);
+
+      //Cone
+      this.cone.rotation.set(0.5, 1, 0);
+      this.scene.add(this.cone);
+
+      //Ligth
+      this.scene.add(this.AmbientalLigth);
+      this.DirectionalLigth.position.set(3, 3, 3);
+      this.scene.add(this.DirectionalLigth);
+
+      //Controls
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      this.controls.enableDamping = true;
+    },
     animate() {
       this.renderer.render(this.scene, this.camera);
       requestAnimationFrame(this.animate);
@@ -114,13 +118,39 @@ export default {
     changeTexture() {
       // Click interaction
       var canvas = document.getElementsByTagName("canvas")[0];
-
+      var material = this.material;
+      var arr = this.arr;
+      var textureToShow= this.textureToShow;
       canvas.addEventListener("click", function () {
-        var loader = new THREE.TextureLoader();
-        loader.crossOrigin = '';  //allow cross origin loading
-        console.log('material:::', this.material);
-        this.material.map = loader.load('https://s3-us-west-2.amazonaws.com/s.cdpn.io/259155/THREE_gates.jpg');
+        //LOAD TEXTURE and on completion apply it on SPHERE
+        new THREE.TextureLoader().load(
+          arr[textureToShow],
+          texture => {
+            //Update Texture
+            material.map = texture;
+            material.needsUpdate = true;
+            // Update the next texture to show
+            textureToShow++;
+            // Have we got to the end of the textures array
+            if (textureToShow > arr.length - 1) {
+              textureToShow = 0;
+            }
+          },
+          xhr => {
+            //Download Progress
+            console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+          },
+          error => {
+            //Error CallBack
+            console.log("An error happened" + error);
+          }
+        );
       });
+
+      this.material = material;
+      this.textureToShow= textureToShow;
+      this.cone = new THREE.Mesh(this.geometry, this.material);
+      this.created();
     }
   },
 };
