dfddb8041934cbb148f4a9125a4918b66bec980b
Daniela Buitrago <lauridani1@gmail.com>
2022-07-12 16:53:37 -0500

    mejora del codigo

--- src/components/Molecules/MenuLeft.vue
+++ src/components/Molecules/MenuLeft.vue
@@ -1,17 +1,33 @@
 <template>
   <v-footer color="#fff" class="menuLeft pa-0" padless>
     <v-col cols="12" class="mx-0 my-0" justify="center" no-gutters>
-        <v-btn-toggle v-model="text" tile color="bluedark accent-3" group>
-          <v-btn
-            class="rounded-lg"
-            v-for="childrens in optionsInfo"
-            :key="childrens.id"
-            @click="selected(childrens)"
-            :value="childrens.name"
-          >
-            {{ childrens.name }}
-          </v-btn>
-        </v-btn-toggle>
+      <v-btn-toggle v-model="text" tile color="bluedark accent-3" group>
+        <v-btn class="rounded-lg" v-for="childrens in optionsInfo" :key="childrens.id" @click="selected(childrens)"
+          :value="childrens.name">
+          {{ childrens.name }}
+        </v-btn>
+      </v-btn-toggle>
+      <v-btn color="bluedark" class="ma-auto" dark fab center @click="dialog2 = true">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-dialog v-model="dialog2" max-width="500px">
+        <v-card>
+          <v-card-title>
+            Dialog 2
+          </v-card-title>
+          <v-card-text>
+            <v-btn color="primary" dark @click="dialog3 = !dialog3">
+              Open Dialog 3
+            </v-btn>
+            <v-select :items="select" label="A Select List" item-value="text"></v-select>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" text @click="dialog2 = false">
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-col>
   </v-footer>
 </template>
@@ -57,4 +73,10 @@ export default {
   aling-items: center;
 }
 
+button.ma-auto.v-btn {
+  bottom: 53px;
+  position: absolute;
+  left: 0;
+  right: 0;
+}
 </style>
