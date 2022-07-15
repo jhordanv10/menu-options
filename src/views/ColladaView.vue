920c3e3a422f5198a6ce0de8973b5d38ea164725
Cesar Peña <cesarphernandez09@gmail.com>
2022-07-15 09:58:07 -0500

    Collada loader

--- src/views/ColladaView.vue
+++ src/views/ColladaView.vue
@@ -35,12 +35,11 @@ export default {
     MenuLeft,
   },
   data() {
-    let elf;
     //Scene
     let scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf0f0f0);
 
-    //Camera
+    // //Camera
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
@@ -61,16 +60,6 @@ export default {
     let cube = new THREE.Mesh(geometry, material);
     cube.name = "cube";
 
-    // loading manager
-    const loadingManager = new THREE.LoadingManager(function () {
-      scene.add(elf);
-    });
-
-    const loader = new ColladaLoader(loadingManager);
-    loader.load("../../public/collada/collada.dae", function (collada) {
-      elf = collada.scene;
-    });
-
     //Ligths
     const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
     scene.add(ambientLight);
@@ -90,18 +79,21 @@ export default {
 
   created() {
     //Camera
-    this.camera.position.z = -10;
+    //this.camera.position.z = -10;
     this.scene.add(this.camera);
 
+
     //Cube
-    this.scene.add(this.cube);
 
     //Controls
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
     this.controls.enableDamping = true;
+
+    this.colladaRender()
+        
   },
 
-  mounted() {
+  async mounted() {
     this.$refs.canvas.appendChild(this.renderer.domElement);
     this.animate();
   },
@@ -110,8 +102,60 @@ export default {
     animate() {
       this.renderer.render(this.scene, this.camera);
       requestAnimationFrame(this.animate);
+      this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+      this.controls.enableDamping = true;
       this.controls.update();
     },
+    colladaRender() {
+      const texture = new THREE.TextureLoader().load( require("../assets//water.png" ));
+      const loader = new ColladaLoader();
+      let camera2 = null;
+      let scene = this.scene;
+      let camera = this.camera;
+      loader.load("./collada.dae", function (collada) {
+        let mockup = collada.scene;
+        mockup.traverse( async function (node) {
+          if (node.isMesh) {
+            console.log(node)
+            node.material.color = new THREE.Color(0xffffff);
+            node.material.blending = THREE.NoBlending;
+            node.material.alphaTest = 0.5;
+            node.material.transparent = false;
+            // node.material.side = THREE.DoubleSide; // Enable back-faces
+            if (node.material.name == 'pet-label') {
+              node.material.map = texture;
+            }
+          }
+        });
+        scene.add(mockup)
+        camera = camera
+        let camera2 = scene.getObjectByName('Camera');
+        console.log(camera2)
+
+        if (!camera2) {
+          console.error({message: "Camera not found"});
+        }
+
+        camera2.aspect = 5000 / 3333;
+        if (camera2.type == "OrthographicCamera") {
+          camera2.zoom = scene.getObjectByName("zoom").position.x * 100 || 20;
+          camera2.left = -camera2.aspect;
+          camera2.right = camera2.aspect;
+          camera2.top = 1;
+          camera2.bottom = -1;
+        }
+        camera2.updateProjectionMatrix();
+        scene.add(camera2);
+
+      });
+      setTimeout(() => {
+        console.log(camera2)
+        console.log(this.scene)
+        console.log(this.camera)
+        this.camera  = this.scene.children[4]
+      }, 500);
+      console.log("Camera 2")
+    }
   },
 };
 </script>
